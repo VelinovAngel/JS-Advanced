0aa6eef17651b405a53f85ddc8af5ae2a59bdbed
VelinovAngel <kakarum1992@gmail.com>
2021-09-20 23:07:07 +0200

    Piece of pie

---
+++ JS Advanced/ArraysAndNestedArrays/7.Piece-of-Pie.js
@@ -0,0 +1,26 @@
+function pieceOfPie(input, startArr, endArr) {
+    let startIndex = input.indexOf(startArr);
+    let endIndex = input.indexOf(endArr);
+
+    return input.slice(startIndex, endIndex + 1);
+}
+
+
+console.log(pieceOfPie(['Pumpkin Pie',
+    'Key Lime Pie',
+    'Cherry Pie',
+    'Lemon Meringue Pie',
+    'Sugar Cream Pie'],
+    'Key Lime Pie',
+    'Lemon Meringue Pie'
+));
+
+console.log(pieceOfPie(['Apple Crisp',
+    'Mississippi Mud Pie',
+    'Pot Pie',
+    'Steak and Cheese Pie',
+    'Butter Chicken Pie',
+    'Smoked Fish Pie'],
+    'Pot Pie',
+    'Smoked Fish Pie'
+))
