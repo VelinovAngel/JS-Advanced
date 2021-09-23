431d72c49e5db4dcb2cbfd71c0948ac93e17cf87
VelinovAngel <kakarum1992@gmail.com>
2021-09-23 14:00:20 +0200

    List of names

--- JS Advanced/ArraysAndNestedArrays/6.List-of-Names.js
+++ JS Advanced/ArraysAndNestedArrays/6.List-of-Names.js
@@ -1,6 +1,10 @@
-function listOfName(input){
-
+function listOfName(input) {
+    let newArr = input.sort((a,b) => a.localeCompare(b));
+    for (let i = 0; i < newArr.length; i++) {
+        console.log(`${i + 1}.${newArr[i]}`);
+    }
 }
 
 
-console.log(listOfName(["John", "Bob", "Christina", "Ema"]));
+listOfName(["John", "Bob", "Christina", "Ema"]);
+listOfName([""]);
