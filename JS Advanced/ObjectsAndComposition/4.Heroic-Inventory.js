e27019552b251fddeaaa9f219380298ff7dabbb3
VelinovAngel <kakarum1992@gmail.com>
2021-09-26 21:03:34 +0200

    Heroic Inventory

---
+++ JS Advanced/ObjectsAndComposition/4.Heroic-Inventory.js
@@ -0,0 +1,20 @@
+function heroicInventory(input) {
+    let result = [];
+
+    for (const item of input) {
+        let [name, level, items] = item.split(' / ');
+        name = name;
+        level = Number(level);
+        result.push({
+            name,
+            level: level,
+            items: items ? items.split(', ') : [],
+        });
+    }
+    return JSON.stringify(result);
+}
+
+console.log(heroicInventory(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']
+));
