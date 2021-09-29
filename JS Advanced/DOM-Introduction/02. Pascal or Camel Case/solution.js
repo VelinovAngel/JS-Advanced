d29201b85513e580bdded949018080019058a5e9
VelinovAngel <kakarum1992@gmail.com>
2021-09-29 20:48:52 +0200

    Exercise 2 Solution

--- JS Advanced/DOM-Introduction/02. Pascal or Camel Case/solution.js
+++ JS Advanced/DOM-Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let command = document.getElementById('naming-convention').value;
+
+  if (command.includes('Camel Case')) {
+    document.getElementById('result').textContent = input = toCamelCase(input);
+    input = toCamelCase(input);
+    function toCamelCase(str) {
+      return str.split(' ').map(function (word, index) {
+        if (index == 0) {
+          return word.toLowerCase();
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      }).join('');
+    }
+  } else if (command.includes('Pascal Case')) {
+    document.getElementById('result').textContent = input = toPascalCase(input);
+    input = toPascalCase(input);
+    function toPascalCase(str) {
+      return str.split(' ').map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      }).join('');
+    }
+  } else {
+    document.getElementById('result').textContent = 'Error!';
+  }
 }
