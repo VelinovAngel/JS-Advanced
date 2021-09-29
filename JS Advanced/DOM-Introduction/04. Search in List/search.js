5a9501bae18297866b5b71f0a9213bca472fbd99
VelinovAngel <kakarum1992@gmail.com>
2021-09-29 22:53:17 +0200

    Exercise 4 Search

--- JS Advanced/DOM-Introduction/04. Search in List/search.js
+++ JS Advanced/DOM-Introduction/04. Search in List/search.js
@@ -1,3 +1,13 @@
 function search() {
-   // TODO
+   let elements = document.getElementById('towns').children;
+   let search = document.getElementById('searchText').value;
+   let count = 0;
+   for (const element of Array.from(elements)) {
+      if(element.textContent.includes(search)){
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration  = 'underline';
+         count++;
+      }
+   }
+   document.getElementById('result').innerHTML = `${count} matches found`;
 }
