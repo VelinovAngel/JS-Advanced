845e69585f8c4d777cbf854f20ef210b2209a70d
VelinovAngel <kakarum1992@gmail.com>
2021-09-29 21:50:22 +0200

    Exercise 3 Accordion

--- JS Advanced/DOM-Introduction/03. Accordion/accordion.js
+++ JS Advanced/DOM-Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const state = document.getElementsByClassName('button')[0];
+    let element = document.getElementById('extra');
+    if (state.innerHTML == 'More') {
+        state.innerHTML = 'Less';
+        element.style.display = 'block';
+    } else {
+        state.innerHTML = 'More';
+        element.style.display = 'none';
+    }
+
 }
