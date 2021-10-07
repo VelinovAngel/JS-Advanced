8800fbd6313c689fb99446b7c1e976adde2d3950
VelinovAngel <kakarum1992@gmail.com>
2021-10-07 19:35:21 +0200

    02. Delete from Table

--- JS Advanced/DOM-Manipulations-and-Events/Lab/02. Delete-From-Table/app.js
+++ JS Advanced/DOM-Manipulations-and-Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,13 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let elements = Array.from(document.querySelector('tbody').children);
+    let input = document.querySelector('input[name="email"]');
+
+    for (const item of elements) {
+        if (item.children[1].textContent == input.value) {
+            item.remove(item);
+            document.getElementById('result').textContent = 'Deleted.';
+        }else{
+            document.getElementById('result').textContent = 'Not found.';
+        }
+    }
 }
