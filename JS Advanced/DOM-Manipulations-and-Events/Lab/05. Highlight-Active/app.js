8bbc19c13c057e681583f962b73336de00e886e0
VelinovAngel <kakarum1992@gmail.com>
2021-10-08 10:37:03 +0200

    05.Hightlight active

--- JS Advanced/DOM-Manipulations-and-Events/Lab/05. Highlight-Active/app.js
+++ JS Advanced/DOM-Manipulations-and-Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    const inputs = Array.from(document.getElementsByTagName('input'));
+
+    for (let input of inputs) {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(ev) {
+        ev.target.parentNode.className = 'focused';
+    }
+
+    function onBlur(ev) {
+        ev.target.parentNode.className = '';
+    }
 }
