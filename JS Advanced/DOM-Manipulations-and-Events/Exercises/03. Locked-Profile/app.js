6fbc77634899d8ffefde4ded7153b898b8ce917b
VelinovAngel <kakarum1992@gmail.com>
2021-10-09 21:19:41 +0200

    4.Fill Dropdown score 100/100

--- JS Advanced/DOM-Manipulations-and-Events/Exercises/03. Locked-Profile/app.js
+++ JS Advanced/DOM-Manipulations-and-Events/Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button'))
+        .forEach(b => b.addEventListener('click', onToggle));
+
+
+    function onToggle(ev) {
+        const profile = ev.target.parentElement;
+        const isActive = profile
+            .querySelector('input[type="radio"][value="unlock"]').checked;
+        console.log(isActive);
+        const info = Array.from(profile.querySelectorAll('div'))
+            .find(el => el.id.includes('HiddenFields'));
+
+        if (isActive) {
+            if (ev.target.textContent == 'Show more') {
+                info.style.display = 'block';
+                ev.target.textContent = 'Hide it'
+            } else {
+                info.style.display = '';
+                ev.target.textContent = 'Show more'
+            }
+        }
+        console.log(info);
+    }
 }
--- JS Advanced/DOM-Manipulations-and-Events/Exercises/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM-Manipulations-and-Events/Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputText = document.getElementById('newItemText');
+    const inputValue = document.getElementById('newItemValue');
+    const selectMenu = document.getElementById('menu');
+
+    // console.log(inputText);
+    // console.log(inputValue);
+    const option = document.createElement('option');
+    selectMenu.appendChild(option);
+    option.textContent = inputText.value;
+    option.value = inputValue.value;
+    inputText.value = '';
+    inputValue.value = '';
 }
