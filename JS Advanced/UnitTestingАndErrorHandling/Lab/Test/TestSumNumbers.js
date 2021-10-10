f20fedf236217cc45f1a5468814fb7cfa0ad06d7
VelinovAngel <kakarum1992@gmail.com>
2021-10-10 22:23:21 +0200

    04. Sum numbers and test it score 100/100

---
+++ JS Advanced/UnitTestingАndErrorHandling/Lab/Test/TestSumNumbers.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const sum = require('../04.SumNumbers');
+
+describe('Demo test', () => {
+    it('works with 5 and 3', () => {
+        expect(sum(['5', '3'])).to.equal(8);
+    });
+    it('Should return 0 ', () => {
+        expect(sum([])).to.be.equal(0);
+    });
+
+    it('Should return NaN', () => {
+        expect(sum('invalid date')).to.be.NaN;
+    });
+
+    it('Should return 7', () => {
+        expect(sum([1.5, 4, 3, -1.5])).to.be.equal(7);
+    });
+
+    it('Should return [1]', () => {
+        expect(sum([1])).to.be.equal(1);
+    });
+});
