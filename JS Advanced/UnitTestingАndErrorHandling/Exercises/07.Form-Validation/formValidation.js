7ea046ed1b294ad3741ed2f381f04c88b7672203
VelinovAngel <kakarum1992@gmail.com>
2021-10-12 18:42:59 +0200

    Score 100/100

--- JS Advanced/UnitTestingАndErrorHandling/Exercises/07.Form-Validation/formValidation.js
+++ JS Advanced/UnitTestingАndErrorHandling/Exercises/07.Form-Validation/formValidation.js
@@ -1,82 +1,73 @@
-    function validate() {
-        const userNameReg = /(^[a-zA-z0-9]{3,20}$)/;
-        const passReg = /(^[\w]{5,15}$)/;
-        const emailReg = /^[\w]+@[\w]+\.[\w]+$/;
-
-        let isValideInput = true;
-        let isChecked = false;
-
-        const submitBtn = document.getElementById('submit');
-        submitBtn.addEventListener('click', onClick);
-
-        function onClick(ev) {
-            ev.preventDefault();
-            const userName = document.getElementById('username');
-            if (!userNameReg.test(userName.value)) {
-                isValideInput = false;
-                userName.style.borderColor = 'red';
-            } else {
-                userName.style.borderColor = 'none';
-            }
-
-            const password = document.getElementById('password');
-            const confirmPass = document.getElementById('confirm-password');
-            if (!passReg.test(password.value)) {
-                isValideInput = false;
-                password.style.borderColor = 'red';
-                confirmPass.style.borderColor = 'red';
-            }
-            else {
-                password.style.borderColor = 'none';
-                confirmPass.style.borderColor = 'none';
-            }
-
-            if (password.value != confirmPass.value) {
-                isValideInput = false;
-                confirmPass.style.borderColor = 'red';
-            }
-            else {
-                confirmPass.style.borderColor = 'none';
-            }
-
-            const email = document.getElementById('email');
-
-            if (!emailReg.test(email.value)) {
-                isValideInput = false;
-                email.style.borderColor = 'red';
-            }
-            else {
-                email.style.borderColor = 'none';
-            }
-
-            const validDiv = document.getElementById('valid');
-            if (isValideInput) {
-                validDiv.style.display = 'block';
-            } else {
-                validDiv.style.display = 'none';
-            }
-
-            if (isChecked) {
-                const companyNumber = document.getElementById('companyNumber');
-                if (Number(companyNumber.value) < 1000 
-                || Number(companyNumber.value) > 9999) {
-                    companyNumber.style.borderColor = 'red';
-                }
-                else {
-                    companyNumber.style.borderColor = 'none';
-                }
-            }
+function validate() {
+    document.querySelector("#submit").type = "button";
+    document.querySelector("#company").addEventListener("change", (e) => {
+      console.log(e.target);
+      if (document.querySelector("#company").checked) {
+        document.querySelector("#companyInfo").style.display = "block";
+      } else {
+        document.querySelector("#companyInfo").style.display = "none";
+      }
+    });
+  
+    document.querySelector("#submit").addEventListener("click", (e) => {
+      let validOut = [];
+      let userName = document.querySelector("#username");
+      let email = document.querySelector("#email");
+      let passWord = document.querySelector("#password");
+      let confirmPass = document.querySelector("#confirm-password");
+      let checkBox = document.querySelector("#company");
+      let userTest = /^[A-Za-z0-9]{3,20}$/;
+      let emailTest = /^[^@.]+@[^@]*\.[^@]*$/;
+      let passTest = /^[\w]{5,15}$/;
+  
+      //console.log(checkBox.checked);
+  
+      if (userTest.exec(userName.value) === null) {
+        userName.style.borderColor = "red";
+        validOut.push(false);
+      } else {
+        userName.style.borderColor = "";
+        validOut.push(true);
+      }
+  
+      if (emailTest.exec(email.value) === null) {
+        email.style.borderColor = "red";
+        validOut.push(false);
+      } else {
+        email.style.borderColor = "";
+        validOut.push(true);
+      }
+  
+      if (
+        passWord.value === confirmPass.value &&
+        passTest.exec(confirmPass.value) != null &&
+        passTest.exec(passWord.value) != null
+      ) {
+        confirmPass.style.borderColor = "";
+        passWord.style.borderColor = "";
+        validOut.push(true);
+      } else {
+        confirmPass.style.borderColor = "red";
+        passWord.style.borderColor = "red";
+        validOut.push(false);
+      }
+  
+      if (checkBox.checked) {
+        let company = document.querySelector("#companyNumber");
+        if (company.value < 1000 || company.value > 9999) {
+          company.style.borderColor = "red";
+          validOut.push(false);
+        } else {
+          company.style.borderColor = "";
+          validOut.push(true);
         }
-
-        document.getElementById('company').addEventListener('change', onChange);
-        function onChange(ev) {
-            const company = document.getElementById('companyInfo');
-            if (ev.target.checked) {
-                isChecked = true;
-                company.style.display = 'block';
-            } else {
-                isChecked = false;
-                company.style.display = 'none';
-            }
-        }
-    }
+      }
+  
+      if (!validOut.includes(false)) {
+        document.querySelector("#valid").style.display = "block";
+      } else {
+        document.querySelector("#valid").style.display = "none";
+      }
+    });
+  }
+  
