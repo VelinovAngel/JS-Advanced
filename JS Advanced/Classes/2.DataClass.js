8d5033352007edd9f11a8fbd3989bbbc93b565c6
VelinovAngel <kakarum1992@gmail.com>
2021-10-15 12:29:29 +0200

    2.DataClass score 100/100

--- JS Advanced/Classes/2.DataClass.js
+++ JS Advanced/Classes/2.DataClass.js
@@ -1,11 +1,11 @@
 class Request {
-    constructor(method, uri, version, message, response, fulfilled = false){
+    constructor(method, uri, version, message){
         this.method = method,
         this.uri = uri,
         this.version = version,
         this.message = message,
-        this.response = response,
-        this.fulfilled = fulfilled
+        this.response = undefined,
+        this.fulfilled = false
     }
 }
 
