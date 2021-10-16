c5216467d0573bd6f353791e654d10cc0eca6ee8
VelinovAngel <kakarum1992@gmail.com>
2021-10-17 00:32:16 +0200

    12.PaymentPackage score 100/100

--- JS Advanced/Classes/12.PaymentPackage.js
+++ JS Advanced/Classes/12.PaymentPackage.js
@@ -74,20 +74,63 @@ class PaymentPackage {
 }
 
 
-describe('PaymentPackage tests', ()=>{
-    it('constructor' , ()=>{
+describe('PaymentPackage tests', () => {
+    let instance = undefined;
+    beforeEach(() => {
+        instance = new PaymentPackage('Name', 100)
+    })
+    it('constructor', () => {
+        assert.equal(instance._name, 'Name');
+        assert.equal(instance._value, 100);
+        assert.equal(instance._VAT, 20);
+        assert.equal(instance._active, true);
+    })
+    it('name', () => {
+        assert.equal(instance.name, 'Name');
+        instance.name = 'Pesho';
+        assert.equal(instance.name, 'Pesho');
+        assert.throw(() => { instance.name = '' }, 'Name must be a non-empty string');
+        assert.throw(() => { instance.name = 2 }, 'Name must be a non-empty string');
+    })
+    it('VAT', () => {
+        assert.equal(instance.VAT, 20);
+        instance.VAT = 40;
+        assert.equal(instance.VAT, 40);
+        assert.throw(() => { instance.VAT = '' }, 'VAT must be a non-negative number');
+        assert.throw(() => { instance.VAT = -1 }, 'VAT must be a non-negative number');
+    })
+    it('Active', () => {
+        assert.equal(instance.active, true);
+        instance.active = false;
+        assert.equal(instance.active, false);
+        assert.throw(() => { instance.active = '00' }, 'Active status must be a boolean');
+        assert.throw(() => { instance.active = 0 }, 'Active status must be a boolean');
+    })
+    it('value', () => {
+        assert.equal(instance.value, 100);
+        instance.value = 40;
+        assert.equal(instance.value, 40);
+        assert.throw(() => { instance.value = '' }, 'Value must be a non-negative number');
+        assert.throw(() => { instance.value = 'string' }, 'Value must be a non-negative number');
+        assert.throw(() => { instance.value = undefined }, 'Value must be a non-negative number');
+        assert.throw(() => { instance.value = -1 }, 'Value must be a non-negative number');
+        assert.doesNotThrow(() => { instance.value = 0 }, 'Value must be a non-negative number');
+    })
+    it('ToString', () => {
+        function getString(name, value, VAT = 20, active = true) {
+            return [`Package: ${name}` + (active === false ? ' (inactive)' : ''),
+            `- Value (excl. VAT): ${value}`,
+            `- Value (VAT ${VAT}%): ${value * (1 + VAT / 100)}`].join('\n');
+        }
 
-    })
-    it('name' , ()=>{
-        
-    })
-    it('VAT' , ()=>{
-        
-    })
-    it('Active' , ()=>{
-        
-    })
-    it('ToString' , ()=>{
-        
+        assert.equal(instance.toString(), getString('Name', 100));
+        instance.active = false;
+        assert.equal(instance.toString(), getString('Name', 100, 20, false));
+        instance.VAT = 200;
+        assert.equal(instance.toString(), getString('Name', 100, 200, false));
+        instance.name = 'Gosho';
+        assert.equal(instance.toString(), getString('Gosho', 100, 200, false));
+        instance.value = 2;
+        assert.equal(instance.toString(), getString('Gosho', 2, 200, false));
     })
 })
