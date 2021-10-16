3b46eda7b089e2ad8317579fa0c732a890428284
VelinovAngel <kakarum1992@gmail.com>
2021-10-16 19:31:18 +0200

    8.HuiceFlavors score 100/100

---
+++ JS Advanced/Classes/8.JuiceFlavors.js
@@ -0,0 +1,54 @@
+function juiceFlavors(input) {
+
+    let obj = {};
+    let result = {};
+
+    for (let item of input) {
+        let [juice, value] = item.split(' => ');
+
+        if (!obj[juice]) {
+            obj[juice] = Number(value);
+            produceBottle(value, juice);
+        }
+        else {
+            obj[juice] += Number(value);
+            produceBottle(value, juice);
+        }
+
+    }
+
+    function produceBottle(value, juice) {
+        let currValue = obj[juice];
+        while (currValue - 1000 >= 0) {
+            currValue -= 1000;
+            if(!result[juice]){
+                result[juice] = 1;
+            }
+            else{
+                result[juice]++;
+            }
+        }
+        obj[juice] = currValue;
+    }
+
+    for(let [key, value] of Object.entries(result)){
+        console.log(`${key} => ${value}`)
+    }
+}
+
+
+
+juiceFlavors(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549']
+);
+
+juiceFlavors(['Kiwi => 234',
+    'Pear => 2345',
+    'Watermelon => 3456',
+    'Kiwi => 4567',
+    'Pear => 5678',
+    'Watermelon => 6789']
+);
